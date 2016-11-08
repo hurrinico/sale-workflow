bcf0ef0a014bb3a7db46076fbc83b45ee36e0c2f
Andrea Cometa <info@andreacometa.it>
2016-11-08 11:43:12 +0100

    [add] new module sale_order_weight

--- sale_order_weight/tests/test_sale_order_weight.py
+++ sale_order_weight/tests/test_sale_order_weight.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+# © 2016 Andrea Cometa - Apulia Software
+# License GPL-3.0 or later (http://www.gnu.org/licenses/gpl.html).
+
 import openerp.tests.common as common
 
 class TestSaleOrderWeight(common.TransactionCase):
